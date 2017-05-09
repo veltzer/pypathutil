24a7d8f3a0945c38baad0b51f547af864005ca3e
Mark Veltzer <mark.veltzer@gmail.com>
2017-05-09 11:05:13 +0300

    first version

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+import sys
+if not sys.version_info[0] == 3:
+    sys.exit("Sorry, only python version 3 is supported")
+
+setuptools.setup(
+    name='pypathutil',
+    version='0.0.1',
+    description='pypathutil is a module to help you deal with paths',
+    long_description='pypathutil is a module to help you deal with paths',
+    url='https://veltzer.github.io/pypathutil',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='python path utils bash',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+    install_requires=[
+        'click',  # for command line parsing
+        'pyfakeuse'  # for fake use of variables
+    ],
+    entry_points={
+        'console_scripts': [
+            'pypathutil_add=pypathutil.scripts.add:main',
+        ],
+    },
+)
---
+++ src/pypathutil/scripts/add.py
@@ -0,0 +1,9 @@
+"""
+This script adds two paths together
+"""
+
+import click
+
+@click.command()
+def main():
+    pass
