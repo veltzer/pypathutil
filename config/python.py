877c85674e814492d255eafb2a8cfdb172dbf0af
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-02 00:01:10 +0300

    new version with templating

---
+++ config/python.py
@@ -0,0 +1,37 @@
+import config.project
+
+package_name = config.project.project_name
+
+console_scripts = [
+    'pypathutil_add=pypathutil.scripts.add:main',
+    'pypathutil_clean=pypathutil.scripts.clean:main',
+    'pypathutil_remove=pypathutil.scripts.remove:main',
+]
+
+setup_requires = [
+]
+
+run_requires = [
+    'pylogconf',  # for nicer logging
+    'pytconf',  # for nicer logging
+]
+
+test_requires = [
+    'pytest',  # for testing
+    'pytest-cov',  # for test coverage
+    'pylint',  # to linting
+    'pyflakes',  # for linting
+    'flake8',  # for linting
+]
+
+dev_requires = [
+    'pyclassifiers',  # for programmatic classifiers
+    'pypitools',  # for upload etc
+    'pydmt',  # for building
+    'Sphinx',  # for the sphinx builder
+]
+
+install_requires = list(setup_requires)
+install_requires.extend(run_requires)
+
+python_requires = ">=3.4"
--- setup.py
+++ setup.py
@@ -1,34 +1,55 @@
 import setuptools
 
+"""
+The documentation can be found at:
+http://setuptools.readthedocs.io/en/latest/setuptools.html
+"""
 setuptools.setup(
+    # the first three fields are a must according to the documentation
     name='pypathutil',
-    version='0.0.11',
-    description='pypathutil is a module to help you deal with paths',
-    long_description='pypathutil is a module to help you deal with paths',
-    url='https://veltzer.github.io/pypathutil',
-    download_url='https://github.com/veltzer/pypathutil',
+    version='0.0.12',
+    packages=[
+        'pypathutil',
+        'pypathutil.scripts',
+    ],
+    # from here all is optional
+    description='command line utilities to help you work with paths',
+    long_description='command line utilities to help you work with paths',
     author='Mark Veltzer',
     author_email='mark.veltzer@gmail.com',
     maintainer='Mark Veltzer',
     maintainer_email='mark.veltzer@gmail.com',
+    keywords=[
+        'path',
+        'command line',
+        'shell',
+        'reduce',
+        'append',
+        'remove',
+    ],
+    url='https://veltzer.github.io/pypathutil',
+    download_url='https://github.com/veltzer/pypathutil',
     license='MIT',
-    platforms=['python3'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
+    platforms=[
+        'python3',
     ],
-    keywords='python path utils bash',
-    packages=setuptools.find_packages(),
     install_requires=[
-        'click',  # for command line parsing
-        'pyfakeuse'  # for fake use of variables
+        'pylogconf',
+        'pytconf',
     ],
-    entry_points={
-        'console_scripts': [
-            'pypathutil_add=pypathutil.scripts.add:main',
-            'pypathutil_clean=pypathutil.scripts.clean:main',
-            'pypathutil_remove=pypathutil.scripts.remove:main',
-        ],
-    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    data_files=[
+    ],
+    entry_points={'console_scripts': [
+        'pypathutil_add=pypathutil.scripts.add:main',
+        'pypathutil_clean=pypathutil.scripts.clean:main',
+        'pypathutil_remove=pypathutil.scripts.remove:main',
+    ]},
+    python_requires='>=3.4',
 )
