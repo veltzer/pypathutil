19f656af7296e21ca84812a031e4bdbcf7b5fb24
Mark Veltzer <mark.veltzer@gmail.com>
2020-06-06 00:52:40 +0300

    new template

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ import setuptools
 
 setuptools.setup(
     # the first three fields are a must according to the documentation
-    name='pypathutil',
-    version='0.0.12',
+    name="pypathutil",
+    version="0.0.12",
     packages=[
         'pypathutil',
         'pypathutil.endpoints',
     ],
     # from here all is optional
-    description='command line utilities to help you work with paths',
-    long_description='command line utilities to help you work with paths',
-    author='Mark Veltzer',
-    author_email='mark.veltzer@gmail.com',
-    maintainer='Mark Veltzer',
-    maintainer_email='mark.veltzer@gmail.com',
+    description="command line utilities to help you work with paths",
+    long_description="command line utilities to help you work with paths",
+    long_description_content_type="text/x-rst",
+    author="Mark Veltzer",
+    author_email="mark.veltzer@gmail.com",
+    maintainer="Mark Veltzer",
+    maintainer_email="mark.veltzer@gmail.com",
     keywords=[
         'path',
         'command line',
@@ -23,9 +24,9 @@ setuptools.setup(
         'append',
         'remove',
     ],
-    url='https://veltzer.github.io/pypathutil',
-    download_url='https://github.com/veltzer/pypathutil',
-    license='MIT',
+    url="https://veltzer.github.io/pypathutil",
+    download_url="https://github.com/veltzer/pypathutil",
+    license="MIT",
     platforms=[
         'python3',
     ],
@@ -44,8 +45,8 @@ setuptools.setup(
     ],
     data_files=[
     ],
-    entry_points={'console_scripts': [
+    entry_points={"console_scripts": [
         'pypathutil=pypathutil.endpoints.main:main',
     ]},
-    python_requires='>=3.5',
+    python_requires=">=3.5",
 )
