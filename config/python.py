0c98eddd961115e6e5f3904bcdd3e9724702351c
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:12:09 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -3,37 +3,28 @@ import config.project
 package_name = config.project.project_name
 
 console_scripts = [
-    'pypathutil=pypathutil.main:main',
-]
-
-setup_requires = [
+    "pypathutil=pypathutil.main:main",
 ]
 
 run_requires = [
-    'pylogconf',
-    'pytconf',
+    "pylogconf",
+    "pytconf",
 ]
 
 test_requires = [
-    'pytest',
-    'pytest-cov',
-    'pylint',
-    'flake8',
-    'pymakehelper',
+    "pytest",
+    "pytest-cov",
+    "pylint",
+    "flake8",
+    "pymakehelper",
 ]
 
 dev_requires = [
-    'pyclassifiers',
-    'pypitools',
-    'pydmt',
-    'Sphinx',
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
