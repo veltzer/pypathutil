783c4b82d8ed4fe0da2b1b9352eefc7a17f68a68
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 02:53:22 +0200

    new pydmt version

--- config/python.py
+++ config/python.py
@@ -1,22 +1,27 @@
-console_scripts = [
+from typing import List
+
+
+console_scripts: List[str] = [
     "pypathutil=pypathutil.main:main",
 ]
-dev_requires = [
+config_requires: List[str] = []
+dev_requires: List[str] = [
     "pypitools",
 ]
-make_requires = [
+install_requires: List[str] = [
+    "pylogconf",
+    "pytconf",
+]
+make_requires: List[str] = [
     "pymakehelper",
     "pyclassifiers",
     "pydmt",
 ]
-install_requires = [
-    "pylogconf",
-    "pytconf",
-]
-test_requires = [
+test_requires: List[str] = [
     "pytest",
     "pytest-cov",
     "pylint",
     "flake8",
     "mypy",
 ]
+requires = config_requires + install_requires + make_requires + test_requires
